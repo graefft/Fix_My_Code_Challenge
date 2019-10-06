e25eb4fc87c199efff7550f084e0b6e0d3ba33cb
Thomas Graeff <676@holbertonschool.com>
2019-10-06 04:35:22 +0000

    Revert to have height

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,17 +10,17 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
