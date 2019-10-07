e330dcb028e4d17bec22790a479a4c48df8e623b
Thomas Graeff <676@holbertonschool.com>
2019-10-07 03:43:37 +0000

    Update docstrings and change name

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,10 +5,10 @@ Makes a square
 
 
 class Square:
-    '''Square class creation'''
+    """Square class creation"""
 
     def __init__(self, *args, **kwargs):
-        '''makes new square instance'''
+        """makes new square instance"""
         self.width = 0
         self.height = 0
         for key, value in kwargs.items():
@@ -18,16 +18,16 @@ class Square:
         """ Area of the square """
         return self.width * self.height
 
-    def permiterOfMySquare(self):
-        '''Perimeter of Square'''
+    def permiter_of_my_square(self):
+        """Perimeter of Square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        '''Str representation of width and height'''
+        """Str representation of width and height"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiterOfMySquare())
+    print(s.permiter_of_my_square())
